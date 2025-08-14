8c29a16b62af3244a5edcc98cbaffead4c114c9f
Grant Lemons <grantlemons@aol.com>
2025-08-14 11:11:45 -0600

    create tests for AND and OR gates

---
+++ project-1/and.v
@@ -0,0 +1,3 @@
+module student_and(input a, input b, output out);
+  assign out = a & b;
+endmodule
---
+++ project-1/tests/and_test.v
@@ -0,0 +1,27 @@
+module and_test;
+  reg a, b;
+  wire out;
+
+  student_and dut (.a(a), .b(b), .out(out));
+
+  initial begin
+    $display("| a | b |out|");
+
+    a = 0;
+    b = 0;
+    #1 $display("| %d | %d | %d |", a, b, out);
+
+    a = 0;
+    b = 1;
+    #2 $display("| %d | %d | %d |", a, b, out);
+
+    a = 1;
+    b = 0;
+    #3 $display("| %d | %d | %d |", a, b, out);
+
+    a = 1;
+    b = 1;
+    #4 $display("| %d | %d | %d |", a, b, out);
+    $finish;
+  end // initial begin
+endmodule // and_test
