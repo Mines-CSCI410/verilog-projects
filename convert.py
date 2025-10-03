248bbb6c9aaa5af57ba7e0fdb8dd09cf377637a1
Grant Lemons <grantlemons@aol.com>
2025-10-03 09:02:38 -0600

    adapt conversion script padding

--- convert.py
+++ convert.py
@@ -1,7 +1,7 @@
 from sys import argv
 from re import match
 
-labels: list[str] = []
+labels: list[tuple[str, int]] = []
 lines: list[list[str]] = []
 header_string = ""
 with open(argv[1], 'r') as f:
@@ -11,24 +11,28 @@ with open(argv[1], 'r') as f:
         return list(valid_sections)
     header_string = f.readline().strip()
     lines = list(map(split_line, f.readlines()))
-    labels = split_line(header_string)
+    widths = list(map(lambda s: len(s) - 2, filter(lambda s: len(s) > 0, header_string.split('|'))))
+    l = split_line(header_string)
+    labels = list(zip(l, widths))
 
 print(f'$display("{header_string}")')
 print()
 for (test, line) in enumerate(lines):
-    for (i, label) in filter(lambda t: t[1] != 'out' and t[1] != 'time', enumerate(labels)):
+    for (i, (label, _)) in filter(lambda t: t[1] != 'out' and t[1] != 'time', enumerate(labels)):
         if len(line[i]) > 1:
             print(f'{label} = \'b{line[i]};')
         else:
             print(f'{label} = {line[i]};')
 
-    def strings(labels: list[str]) -> tuple[str, str]:
-        fmt_string = '|' + (' %d |' * len(labels))
-        label_string = ", ".join(str(i) for i in labels)
+    def strings(labels: list[tuple[str, int]]) -> tuple[str, str]:
+        fmt_string = '|'
+        for (_, w) in labels:
+            fmt_string += f' {'%d':<{w+1}} |'
+        label_string = ", ".join(str(l) for (l, _) in labels)
         return (fmt_string, label_string)
 
-    if labels[0] == 'time':
-        fmt_string, label_string = strings(list(filter(lambda s: s != 'time', labels)))
+    if labels[0][0] == 'time':
+        fmt_string, label_string = strings(list(filter(lambda s: s[0] != 'time', labels)))
         if match('\\d+\\+', line[0]):
             time = int(line[0].removesuffix('+'))
             t_str = str(time) + '+'
