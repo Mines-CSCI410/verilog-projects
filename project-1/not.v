554fa3f11ce23de6ff32d2683652eeb6374f3b48
Grant Lemons <grantlemons@aol.com>
2025-10-16 09:55:06 -0600

    project 1 readme

---
+++ project-1/not.v
@@ -0,0 +1,8 @@
+module student_not(input in, output out);
+    /*
+    * Develop your solution here.
+    * You can use NAND (nand_gate) or any gate you've already created
+    * (student_xyz).
+    * Please don't use built-in versions of the gate you are modeling.
+    */
+endmodule
