6247da7ec47e8fb1d5e3b2b676a9f62f098d1fd9
Grant Lemons <grantlemons@aol.com>
2025-10-30 16:19:02 -0600

    fix dmux4way test and add test failure message

--- project-1/tests/dmux4way_test.v
+++ project-1/tests/dmux4way_test.v
@@ -10,66 +10,34 @@ module dmux4way_test;
 
     in = 0;
     sel = 'b00;
-    a = 0;
-    b = 0;
-    c = 0;
-    d = 0;
     #1 $display("|%b|%b|%b|%b|%b|%b|", in, sel, a, b, c, d);
 
     in = 0;
     sel = 'b01;
-    a = 0;
-    b = 0;
-    c = 0;
-    d = 0;
     #2 $display("|%b|%b|%b|%b|%b|%b|", in, sel, a, b, c, d);
 
     in = 0;
     sel = 'b10;
-    a = 0;
-    b = 0;
-    c = 0;
-    d = 0;
     #3 $display("|%b|%b|%b|%b|%b|%b|", in, sel, a, b, c, d);
 
     in = 0;
     sel = 'b11;
-    a = 0;
-    b = 0;
-    c = 0;
-    d = 0;
     #4 $display("|%b|%b|%b|%b|%b|%b|", in, sel, a, b, c, d);
 
     in = 1;
     sel = 'b00;
-    a = 1;
-    b = 0;
-    c = 0;
-    d = 0;
     #5 $display("|%b|%b|%b|%b|%b|%b|", in, sel, a, b, c, d);
 
     in = 1;
     sel = 'b01;
-    a = 0;
-    b = 1;
-    c = 0;
-    d = 0;
     #6 $display("|%b|%b|%b|%b|%b|%b|", in, sel, a, b, c, d);
 
     in = 1;
     sel = 'b10;
-    a = 0;
-    b = 0;
-    c = 1;
-    d = 0;
     #7 $display("|%b|%b|%b|%b|%b|%b|", in, sel, a, b, c, d);
 
     in = 1;
     sel = 'b11;
-    a = 0;
-    b = 0;
-    c = 0;
-    d = 1;
     #8 $display("|%b|%b|%b|%b|%b|%b|", in, sel, a, b, c, d);
   end
 endmodule
