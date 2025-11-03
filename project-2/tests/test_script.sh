bf9ced7f1d2f67596f23e0cca8e204ff1c10e832
Grant Lemons <grantlemons@aol.com>
2025-11-02 19:19:41 -0700

    rename port to carry from car

--- project-2/tests/test_script.sh
+++ project-2/tests/test_script.sh
@@ -16,7 +16,7 @@ do
 
     iverilog -o /tmp/${TEST}_test.vvp ${TEST}_test.v ../*.v
     vvp /tmp/${TEST}_test.vvp 2> /dev/null 1> /tmp/${TEST}.out 2> /dev/null
-    diff /tmp/${TEST}.out expected-outputs/${TEST}.cmp -qsw --strip-trailing-cr &> /dev/null && echo "${TEST^^} Test Passed"
+    diff /tmp/${TEST}.out expected-outputs/${TEST}.cmp -qsw --strip-trailing-cr &> /dev/null && echo "${TEST^^} Test Passed" || echo "${TEST^^} Test Failed"
 done
 
 popd &> /dev/null
