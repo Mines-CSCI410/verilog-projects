d872a240b6a9a1b4c9415bdd760db60e6f241f01
Grant Lemons <grantlemons@aol.com>
2026-01-16 13:17:49 -0700

    get rid of xor16

--- project-1/xor.v
+++ project-1/xor.v
@@ -6,15 +6,3 @@ module student_xor (input a, b, output out);
     * Please don't use built-in versions of the gate you are modeling.
     */
 endmodule
-
-/*
- * Using your single-bit solution above, create a 16-bit version here.
- */
-module student_xor16 (input [15:0] a, b, output [15:0] out);
-    genvar i;
-    generate
-        for (i=0; i<16; i=i+1) begin
-            // Use your student_xor gate here for each bit in the vectors
-        end
-    endgenerate
-endmodule
