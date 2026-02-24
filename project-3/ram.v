e8a97c24f84fc23d415699de47a6ebe1763b76e8
Grant Lemons <grantlemons@aol.com>
2026-02-24 14:03:17 -0700

    update starter code & autograder for new fast_ram

--- project-3/ram.v
+++ project-3/ram.v
@@ -1,8 +1,13 @@
 // Read the section of the Verilog Primer (linked to on the Canvas page) about
-// Muxlib. It will be helpful.
+// Muxlib. It will be helpful, though you can also use your multi-way
+// multiplexers from project 1.
 
-// As you implement more of these (especially ram16k) running the tests will
-// be pretty slow. That is expected.
+// If you use your student ram implementations for each of these your solution
+// will probably time out. Instead, use the `fast_ram` parameterized module
+// defined in the tests directory in place of your previous ram gate.
+//
+// For instance, in student_ram64, instead of creating 8 instances of
+// student_ram8, create 8 instances of fast_ram #(8).
 
 module student_ram8 (input [15:0] in, input load, input [2:0] address, output [15:0] out);
 endmodule
